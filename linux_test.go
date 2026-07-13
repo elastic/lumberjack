bfa6e4acf4e2e98ae74d91f328f30e9c55f1361b
Dan Kortschak <dan.kortschak@elastic.co>
2026-07-13 12:27:34 +0930

    fix test races

--- linux_test.go
+++ linux_test.go
@@ -1,52 +1,17 @@
+//go:build linux
 // +build linux
 
 package lumberjack
 
 import (
 	"os"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
 )
 
-func TestMaintainMode(t *testing.T) {
-	currentTime = fakeTime
-	dir := makeTempDir("TestMaintainMode", t)
-	defer os.RemoveAll(dir)
-
-	filename := logFile(dir)
-
-	mode := os.FileMode(0600)
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, mode)
-	isNil(err, t)
-	f.Close()
-
-	l := &Logger{
-		Filename:   filename,
-		MaxBackups: 1,
-		MaxSize:    100, // megabytes
-	}
-	defer l.Close()
-	b := []byte("boo!")
-	n, err := l.Write(b)
-	isNil(err, t)
-	equals(len(b), n, t)
-
-	newFakeTime()
-
-	err = l.Rotate()
-	isNil(err, t)
-
-	filename2 := backupFile(dir)
-	info, err := os.Stat(filename)
-	isNil(err, t)
-	info2, err := os.Stat(filename2)
-	isNil(err, t)
-	equals(mode, info.Mode(), t)
-	equals(mode, info2.Mode(), t)
-}
-
-func TestMaintainOwner(t *testing.T) {
+func TestChown(t *testing.T) {
 	fakeFS := newFakeFS()
 	osChown = fakeFS.Chown
 	osStat = fakeFS.Stat
@@ -54,125 +19,166 @@ func TestMaintainOwner(t *testing.T) {
 		osChown = os.Chown
 		osStat = os.Stat
 	}()
-	currentTime = fakeTime
-	dir := makeTempDir("TestMaintainOwner", t)
-	defer os.RemoveAll(dir)
 
-	filename := logFile(dir)
+	t.Run("maintain_mode", func(t *testing.T) {
+		currentTime = fakeTime
+		dir := makeTempDir("TestMaintainMode", t)
+		defer os.RemoveAll(dir)
 
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
-	isNil(err, t)
-	f.Close()
+		filename := logFile(dir)
 
-	l := &Logger{
-		Filename:   filename,
-		MaxBackups: 1,
-		MaxSize:    100, // megabytes
-	}
-	defer l.Close()
-	b := []byte("boo!")
-	n, err := l.Write(b)
-	isNil(err, t)
-	equals(len(b), n, t)
+		mode := os.FileMode(0600)
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, mode)
+		isNil(err, t)
+		f.Close()
 
-	newFakeTime()
+		l := &Logger{
+			Filename:   filename,
+			MaxBackups: 1,
+			MaxSize:    100, // megabytes
+		}
+		defer l.Close()
+		b := []byte("boo!")
+		n, err := l.Write(b)
+		isNil(err, t)
+		equals(len(b), n, t)
 
-	err = l.Rotate()
-	isNil(err, t)
+		newFakeTime()
 
-	equals(555, fakeFS.files[filename].uid, t)
-	equals(666, fakeFS.files[filename].gid, t)
-}
+		err = l.Rotate()
+		isNil(err, t)
 
-func TestCompressMaintainMode(t *testing.T) {
-	currentTime = fakeTime
+		filename2 := backupFile(dir)
+		info, err := os.Stat(filename)
+		isNil(err, t)
+		info2, err := os.Stat(filename2)
+		isNil(err, t)
+		equals(mode, info.Mode(), t)
+		equals(mode, info2.Mode(), t)
+	})
 
-	dir := makeTempDir("TestCompressMaintainMode", t)
-	defer os.RemoveAll(dir)
+	t.Run("maintain_owner", func(t *testing.T) {
+		currentTime = fakeTime
+		dir := makeTempDir("TestMaintainOwner", t)
+		defer os.RemoveAll(dir)
 
-	filename := logFile(dir)
+		filename := logFile(dir)
 
-	mode := os.FileMode(0600)
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, mode)
-	isNil(err, t)
-	f.Close()
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+		isNil(err, t)
+		f.Close()
 
-	l := &Logger{
-		Compress:   true,
-		Filename:   filename,
-		MaxBackups: 1,
-		MaxSize:    100, // megabytes
-	}
-	defer l.Close()
-	b := []byte("boo!")
-	n, err := l.Write(b)
-	isNil(err, t)
-	equals(len(b), n, t)
+		l := &Logger{
+			Filename:   filename,
+			MaxBackups: 1,
+			MaxSize:    100, // megabytes
+		}
+		defer l.Close()
+		b := []byte("boo!")
+		n, err := l.Write(b)
+		isNil(err, t)
+		equals(len(b), n, t)
 
-	newFakeTime()
+		newFakeTime()
 
-	err = l.Rotate()
-	isNil(err, t)
+		err = l.Rotate()
+		isNil(err, t)
 
-	// we need to wait a little bit since the files get compressed on a different
-	// goroutine.
-	<-time.After(10 * time.Millisecond)
+		ff, ok := fakeFS.fakeFile(filename)
+		if !ok {
+			t.Fatalf("%q not found", filename)
+		}
+		equals(555, ff.uid, t)
+		equals(666, ff.gid, t)
+	})
 
-	// a compressed version of the log file should now exist with the correct
-	// mode.
-	filename2 := backupFile(dir)
-	info, err := os.Stat(filename)
-	isNil(err, t)
-	info2, err := os.Stat(filename2 + compressSuffix)
-	isNil(err, t)
-	equals(mode, info.Mode(), t)
-	equals(mode, info2.Mode(), t)
-}
+	t.Run("compress_maintain_mode", func(t *testing.T) {
+		currentTime = fakeTime
 
-func TestCompressMaintainOwner(t *testing.T) {
-	fakeFS := newFakeFS()
-	osChown = fakeFS.Chown
-	osStat = fakeFS.Stat
-	defer func() {
-		osChown = os.Chown
-		osStat = os.Stat
-	}()
-	currentTime = fakeTime
-	dir := makeTempDir("TestCompressMaintainOwner", t)
-	defer os.RemoveAll(dir)
+		dir := makeTempDir("TestCompressMaintainMode", t)
+		defer os.RemoveAll(dir)
 
-	filename := logFile(dir)
+		filename := logFile(dir)
 
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
-	isNil(err, t)
-	f.Close()
+		mode := os.FileMode(0600)
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, mode)
+		isNil(err, t)
+		f.Close()
 
-	l := &Logger{
-		Compress:   true,
-		Filename:   filename,
-		MaxBackups: 1,
-		MaxSize:    100, // megabytes
-	}
-	defer l.Close()
-	b := []byte("boo!")
-	n, err := l.Write(b)
-	isNil(err, t)
-	equals(len(b), n, t)
+		l := &Logger{
+			Compress:   true,
+			Filename:   filename,
+			MaxBackups: 1,
+			MaxSize:    100, // megabytes
+		}
+		defer l.Close()
+		b := []byte("boo!")
+		n, err := l.Write(b)
+		isNil(err, t)
+		equals(len(b), n, t)
 
-	newFakeTime()
+		newFakeTime()
 
-	err = l.Rotate()
-	isNil(err, t)
+		err = l.Rotate()
+		isNil(err, t)
 
-	// we need to wait a little bit since the files get compressed on a different
-	// goroutine.
-	<-time.After(10 * time.Millisecond)
+		// we need to wait a little bit since the files get compressed on a different
+		// goroutine.
+		<-time.After(10 * time.Millisecond)
 
-	// a compressed version of the log file should now exist with the correct
-	// owner.
-	filename2 := backupFile(dir)
-	equals(555, fakeFS.files[filename2+compressSuffix].uid, t)
-	equals(666, fakeFS.files[filename2+compressSuffix].gid, t)
+		// a compressed version of the log file should now exist with the correct
+		// mode.
+		filename2 := backupFile(dir)
+		info, err := os.Stat(filename)
+		isNil(err, t)
+		info2, err := os.Stat(filename2 + compressSuffix)
+		isNil(err, t)
+		equals(mode, info.Mode(), t)
+		equals(mode, info2.Mode(), t)
+	})
+
+	t.Run("compress_maintain_owner", func(t *testing.T) {
+		currentTime = fakeTime
+		dir := makeTempDir("TestCompressMaintainOwner", t)
+		defer os.RemoveAll(dir)
+
+		filename := logFile(dir)
+
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+		isNil(err, t)
+		f.Close()
+
+		l := &Logger{
+			Compress:   true,
+			Filename:   filename,
+			MaxBackups: 1,
+			MaxSize:    100, // megabytes
+		}
+		defer l.Close()
+		b := []byte("boo!")
+		n, err := l.Write(b)
+		isNil(err, t)
+		equals(len(b), n, t)
+
+		newFakeTime()
+
+		err = l.Rotate()
+		isNil(err, t)
+
+		// we need to wait a little bit since the files get compressed on a different
+		// goroutine.
+		<-time.After(10 * time.Millisecond)
+
+		// a compressed version of the log file should now exist with the correct
+		// owner.
+		filename2 := backupFile(dir)
+		ff, ok := fakeFS.fakeFile(filename2 + compressSuffix)
+		if !ok {
+			t.Fatalf("%q not found", filename2+compressSuffix)
+		}
+		equals(555, ff.uid, t)
+		equals(666, ff.gid, t)
+	})
 }
 
 type fakeFile struct {
@@ -181,6 +187,7 @@ type fakeFile struct {
 }
 
 type fakeFS struct {
+	mu    sync.Mutex
 	files map[string]fakeFile
 }
 
@@ -189,8 +196,17 @@ func newFakeFS() *fakeFS {
 }
 
 func (fs *fakeFS) Chown(name string, uid, gid int) error {
+	fs.mu.Lock()
 	fs.files[name] = fakeFile{uid: uid, gid: gid}
+	fs.mu.Unlock()
 	return nil
+}
+
+func (fs *fakeFS) fakeFile(name string) (f fakeFile, ok bool) {
+	fs.mu.Lock()
+	defer fs.mu.Unlock()
+	f, ok = fs.files[name]
+	return f, ok
 }
 
 func (fs *fakeFS) Stat(name string) (os.FileInfo, error) {
